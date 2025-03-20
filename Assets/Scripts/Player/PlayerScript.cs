8539244f4204d80de008743b3513f1b3919977f4
AbhijeetGoush <114390543+AbhijeetGoush@users.noreply.github.com>
2025-03-20 18:26:37 +0000

    player

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vel = rb.velocity;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            vel.y = speed;
+        }
+
+        rb.velocity = vel;
+    }
+}
