d35ee1e5e1734ce9d6a17abce51bf492ce49a1cc
AbhijeetGoush <114390543+AbhijeetGoush@users.noreply.github.com>
2025-03-22 17:30:52 +0000

    Camera Movement

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed;
+    public float xOffset;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x + xOffset, 0, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
+    }
+}
