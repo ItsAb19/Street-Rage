95c61fa23e6228338c6c10c7a6f20ceb5e63f00f
AbhijeetGoush <114390543+AbhijeetGoush@users.noreply.github.com>
2025-03-24 20:35:20 +0000

    background

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -9,7 +9,7 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, 0, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, transform.position.y, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
 }
