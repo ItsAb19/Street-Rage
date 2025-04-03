509a77cac50ec586344d80238785ba7f1ed4fcd3
PoTheFattyPanda <k2417831@kingston.ac.uk>
2025-04-03 23:04:21 +0100

    HealthBar

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
+
+    public GameObject greenHealthbar;
+    private Image greenHealthbarImage;
+
+    void Start()
+    {
+        greenHealthbarImage = greenHealthbar.GetComponent<Image>();
+        currentHealth = MaxHealth;
+    }
+
+    void OnTriggerEnter2D(Collider2D enemyHitbox)
+    {
+        if (enemyHitbox.tag == "enemyHitbox")
+        {
+            greenHealthbarImage.fillAmount -= 0.2f;
+        }
+    }
+
     float MaxHealth = 100;
     public float currentHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        currentHealth = MaxHealth;
-    }
 
     // Update is called once per frame
     void Update()
