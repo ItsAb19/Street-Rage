2854858ede761d8308478c9afc72d3b53f973172
PoTheFattyPanda <k2417831@kingston.ac.uk>
2025-04-04 21:46:31 +0100

    box Destory

--- Assets/BoxDestory.cs
+++ Assets/BoxDestory.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class BoxDestory : MonoBehaviour
+public class CrateHealth : MonoBehaviour
 {
+    public GameObject scoreObj;
+    ScoreScript scoreScr;
+
+    public float MaxHealth = 50;
+    public float currentHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scoreScr = scoreObj.GetComponent<ScoreScript>();
+        currentHealth = MaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth <= 0)
+        {
+            scoreScr.score += 100;
+            Destroy(this.gameObject);
+        }
     }
 }
